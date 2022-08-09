27eb2cc94331af241d357e6c977ce4ef66290c91
Felipe Vidal <58402197+MultiFe22@users.noreply.github.com>
2022-08-08 22:53:22 -0300

    added uncertainty calculator

--- gy/src/main.rs
+++ gy/src/main.rs
@@ -9,11 +9,24 @@ fn g(y: f64) -> f64 {
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let mut sum = 0.0;
+    let mut sum_g = 0.0;
     let max = 100000000;
+    
     for _ in 0..max {
         let generated_number = rng.gen_range(0.0..0.05);
-        sum += g(generated_number);
+        sum_g += g(generated_number);
     }
-    println!("{:e}", sum / max as f64);
+    let precise_result = sum_g/max as f64;
+    println!("{:e}", precise_result);
+
+    let mut sum_uncertainty = 0.0;
+
+    for _ in 0..max{
+        let generated_number = rng.gen_range(0.0..0.05);
+        sum_uncertainty += (g(generated_number)-precise_result).powf(2.0);
+    }
+
+    let uncertainty = (sum_uncertainty/max as f64).powf(0.5);
+
+    println!("{:e}", uncertainty);
 }
